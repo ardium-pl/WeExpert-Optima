8e7221b28787d31a811016214dde8baa59433535
lolocob7 <lolocob7@gmail.com>
2025-03-04 09:49:27 +0100

    test

--- server/src/routers/xmlRouter.ts
+++ server/src/routers/xmlRouter.ts
@@ -32,12 +32,10 @@ xmlRouter.post('/api/process-xml', async (req: Request<RequestExpressData>, res:
     logger.info("xmlString", xmlString)
     try {
       logger.info("flaskUrl", flaskUrl);
-      axios.get(`${flaskUrl}/health-check`)
-      .then(response => console.log("Flask is reachable:", response.data))
-      .catch(error => console.error("Flask connection error:", error.message));
       const response = await axios.post<FlaskResponse>(`${flaskUrl}/upload-xml`, {
         xmlString,
-      });
+      },
+      {family: 4});
       const flaskResponse = response.data;
       if (flaskResponse.status === 'success')
         return res.status(200).json({
